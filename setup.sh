236032c03f6646694c13b52a0c16901a2b25c326
dzajac <dzajac95@gmail.com>
2023-08-10 00:03:17 -0400

    Just run the dang playbook

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Getting ansible..."
-sudo apt install ansible
-
-echo "TODO: Run basic environment setup here (grab dotfiles, ssh key, etc)
+ansible-playbook main.yml --ask-vault-pass
